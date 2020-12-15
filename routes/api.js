d71ee4ee3bb03638be7cae76ec3ed0d9113fdcad
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-15 07:02:32 -0800

    cannot connect to db?

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,48 @@
-'use strict';
+"use strict";
+const expect = require("chai").expect;
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 module.exports = function (app) {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected..."))
+    .catch((err) => console.log(err));
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+  let issueSchema = new mongoose.Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    open: { type: Boolean, required: true },
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
+    project: String,
+  });
+
+  let Issue = mongoose.model("Issue", issueSchema);
+
+  app
+    .route("/api/issues/:project")
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
