b9bb64ba27cd452288458e83f9ff52af9610bc94
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-16 06:11:48 -0800

    test 4 complete

--- routes/api.js
+++ routes/api.js
@@ -33,6 +33,14 @@ module.exports = function (app) {
 
     .post(function (req, res) {
       let project = req.params.project;
+      if (
+        !req.body.issue_title ||
+        !req.body.issue_text ||
+        !req.body.created_by ||
+        !req.body.open
+      ) {
+        return res.json("Required fields missing from requrest");
+      }
       let newIssue = new Issue({
         issue_title: req.body.issue_title,
         issue_text: req.body.issue_text,
@@ -44,10 +52,37 @@ module.exports = function (app) {
         updated_on: new Date().toUTCString(),
         project: project,
       });
+      newIssue.save((err, savedIssue) => {
+        if (!err && savedIssue) {
+          return res.json(savedIssue);
+        }
+      });
     })
 
     .put(function (req, res) {
       let project = req.params.project;
+      let updateObject = {};
+      Object.keys(req.body).forEach((key) => {
+        if (req.body[key] != "") {
+          updateObject[key] = req.body[key];
+        }
+      });
+      if (Object.keys(updateObject).length < 2) {
+        return res.json("no updated field sent");
+      }
+      updateObject.updated_on = new Date().toUTCString();
+      Issue.findByIdAndUpdate(
+        req.body._id,
+        updateObject,
+        { new: true },
+        (err, updatedIssue) => {
+          if (!err && updatedIssue) {
+            return res.json("successfully updated");
+          } else if (!updatedIssue) {
+            return res.json("could not update " + req.body._id);
+          }
+        }
+      );
     })
 
     .delete(function (req, res) {
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,120 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+let id1 = "";
+let id2 = "";
+
+suite("Functional Tests", function () {
+  suite("POST /api/issues/{project} => object with issue data", () => {
+    test("Every field filled in", (done) => {
+      chai
+        .request(server)
+        .post("/api/issues/test")
+        .send({
+          issue_title: "Title",
+          issue_text: "text",
+          created_by: "Functional Test - Every field filled in",
+          assigned_to: "Chai and Mocha",
+          status_text: "In QA",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, "Title");
+          assert.equal(res.body.issue_text, "text");
+          assert.equal(
+            res.body.created_by,
+            "Functional Test - Every field filled in"
+          );
+          assert.equal(res.body.assigned_to, "Chai and Mocha");
+          assert.equal(res.body.status_text, "In QA");
+          assert.equal(res.body.project, "test");
+          id1 = res.body._id;
+          console.log("id 1 has been set as " + id1);
+          done();
+        });
+    });
+
+    test("Required fields filled in", (done) => {
+      chai
+        .request(server)
+        .post("/api/issues/test")
+        .send({
+          issue_title: "Title",
+          issue_text: "text",
+          created_by: "Functional Test - Every field filled in",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, "Title");
+          assert.equal(res.body.issue_text, "text");
+          assert.equal(
+            res.body.created_by,
+            "Functional Test - Every field filled in"
+          );
+          assert.equal(res.body.assigned_to, "");
+          assert.equal(res.body.status_text, "");
+          assert.equal(res.body.project, "test");
+          id2 = res.body._id;
+          console.log("id 1 has been set as " + id2);
+          done();
+        });
+    });
+
+    test("Missing required fields", (done) => {
+      chai
+        .request(server)
+        .post("/api/issues/test")
+        .send({ issue_title: "Title" })
+        .end((err, res) => {
+          assert.equal(res.body, "Required fields missing from request");
+          done();
+        });
+    });
+  });
+
+  suite("PUT /api/issues/{project} => text", () => {
+    test("No body", (done) => {
+      chai
+        .request(server)
+        .put("/api/issues/test")
+        .send({})
+        .end((err, res) => {
+          assert.equal(res.body, "no updated field sent");
+          done();
+        });
+    });
+
+    test("One field to update", (done) => {
+      chai
+        .request(server)
+        .put("/api/issues/test")
+        .send({
+          _id: id1,
+          issue_text: "new text",
+        })
+        .end((err, res) => {
+          assert.equal(res.body, "successfully updated");
+          done();
+        });
+    });
+
+    test("Multiple fields to update", (done) => {
+      chai
+        .request(server)
+        .put("/api/issues/test")
+        .send({
+          _id: id2,
+          issue_title: "new title",
+          issue_text: "new text",
+        })
+        .end((err, res) => {
+          assert.equal(res.body, "successfully updated");
+          done();
+        });
+    });
+  });
 });
